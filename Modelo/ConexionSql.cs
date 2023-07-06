2a37cfdb68dc7b6036b01542ec51d9b4e781cce3
Somalyboy <44luchomar@gmail.com>
2023-07-05 23:30:37 -0300

    primer commit v2 apppublica

---
+++ Modelo/ConexionSql.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Modelo
+{
+    public abstract class ConexionSql
+    {
+        private readonly string StringDeConexion;
+
+        public ConexionSql()
+        {
+            StringDeConexion = "Server=127.0.0.1;userid=root;password=negritoBD123;database=usuarios";
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(StringDeConexion);
+        }
+    }
+}
---
+++ Modelo/UsuarioModelo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Modelo
+{
+    class UsuarioModelo:ConexionSql
+    {
+        public bool Login(string id,string nombre)
+        {
+            using (var conexion = GetConnection())
+            {
+                conexion.Open();
+            }
+        }
+
+    }
+}
